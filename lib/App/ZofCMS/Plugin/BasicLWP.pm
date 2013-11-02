a3028c7968dd355d961822b36c6411515d6e5b26
Zoffix Znet <zoffixcpan@gmail.com>
2013-11-02 00:51:24 +0000

    Import of ZOFFIX/App-ZofCMS-Plugin-BasicLWP-0.0105 from CPAN.
    gitpan-cpan-distribution: App-ZofCMS-Plugin-BasicLWP
    gitpan-cpan-version:      0.0105
    gitpan-cpan-path:         ZOFFIX/App-ZofCMS-Plugin-BasicLWP-0.0105.tar.gz
    gitpan-cpan-author:       ZOFFIX
    gitpan-cpan-maturity:     released

--- lib/App/ZofCMS/Plugin/BasicLWP.pm
+++ lib/App/ZofCMS/Plugin/BasicLWP.pm
@@ -3,7 +3,7 @@ package App::ZofCMS::Plugin::BasicLWP;
 use warnings;
 use strict;
 
-our $VERSION = '0.0104';
+our $VERSION = '0.0105';
 
 use LWP::UserAgent;
 use base 'App::ZofCMS::Plugin::Base';
@@ -57,6 +57,8 @@ sub _do {
 
 1;
 __END__
+
+=encoding utf8
 
 =head1 NAME
 
